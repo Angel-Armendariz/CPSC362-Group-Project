ea3c2c6b67456e7f86b514faa30c6fe94d07c1a1
Angel Armendariz <angela126509@gmail.com>
2023-03-04 13:47:17 -0800

    made header responsive and made changes

--- my-app/src/Components/Header/Header.jsx
+++ my-app/src/Components/Header/Header.jsx
@@ -1,52 +1,50 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './Header.css';
 import logo from '../../assets/logo';
-import audiowave from '../../assets/audiowave.png'
-import {Link} from 'react-router-dom';
+import { Link } from 'react-router-dom';
 
-const Header = () => (
-  <>
-    <div className='headertopbar'>
-      <form className='head-text'>
-        <li>
-          <a href='https://titanradio.org/live/'><img src={audiowave} /></a>
-        </li>
-        <h1>Official Site For Titan Radio Merch</h1>
-      </form>
+const Header = () => {
+  const [menuOpen, setMenuOpen] = useState(false);
 
-    </div>
-    <div className="header">
-        <Link to="/">
-        <img
-          src={logo}
-          alt="example"
-        />
-      </Link>
-    </div>
-    <div>
-            <Link to="/All"><button className='button-all'>
-              All
-            </button>
-            </Link>
-    </div>
-    <div>
-            <Link to="/Apparel"><button className='button-apparel'>
-              Apparel
-            </button>
-            </Link>
-    </div>
-    <div>
-            <Link to="/Accessories"><button className='button-accessories'>
-              Accessories
-            </button>
-            </Link>
-    </div>
-    <div>
-            <Link to="/Cart"><button className='button-cart'>
-              Cart
-            </button>
-            </Link>
-    </div>
+  const toggleMenu = () => {
+    setMenuOpen(!menuOpen);
+  };
+
+  return (
+    <>
+      <div className='header'>
+        <form className='head-text'>
+          <h1>Official Site For Titan Radio Merch</h1>
+        </form>
+        <Link to='/'>
+          <img className='header-logo' src={logo} alt='example' />
+        </Link>
+        <div
+          className={`hamburger-menu ${menuOpen ? 'open' : ''}`}
+          onClick={toggleMenu}
+        >
+          <span></span>
+          <span></span>
+          <span></span>
+        </div>
+      </div>
+      <div className={`navbar ${menuOpen ? 'open' : ''}`}>
+        <Link to='/All'>
+          <button className='button-all'>All</button>
+        </Link>
+        <Link to='/Apparel'>
+          <button className='button-apparel'>Apparel</button>
+        </Link>
+        <Link to='/Accessories'>
+          <button className='button-accessories'>Accessories</button>
+        </Link>
+        <Link to='/Cart'>
+          <button className='button-cart'>Cart</button>
+        </Link>
+      </div>
     </>
-)
+  );
+};
+
 export default Header;
+
