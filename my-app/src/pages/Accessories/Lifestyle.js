3ede5d8e491b6d3ba6edb8ea323d0de9417e4f7c
Angel Armendariz <angela126509@gmail.com>
2023-03-04 22:47:02 -0800

    fixed responsiveness

--- my-app/src/pages/Accessories/Lifestyle.js
+++ my-app/src/pages/Accessories/Lifestyle.js
@@ -5,6 +5,8 @@ const items = [
   { id: 1, title: 'Item 1', image: 'https://www.clipartmax.com/png/small/180-1809318_picture-300-x-300-pixel.png' },
   { id: 2, title: 'Item 2', image: 'https://www.clipartmax.com/png/small/180-1809318_picture-300-x-300-pixel.png' },
   { id: 3, title: 'Item 3', image: 'https://www.clipartmax.com/png/small/180-1809318_picture-300-x-300-pixel.png' },
+  { id: 4, title: 'Item 4', image: 'https://www.clipartmax.com/png/small/180-1809318_picture-300-x-300-pixel.png' },
+  { id: 5, title: 'Item 5', image: 'https://www.clipartmax.com/png/small/180-1809318_picture-300-x-300-pixel.png' },
 ];
 
 const Lifestyle = () => {
