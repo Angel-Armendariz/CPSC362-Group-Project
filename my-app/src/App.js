fa53f3117d69653afee081d39732da8bb74ccfca
darrenjcruz <81451114+darrenjcruz@users.noreply.github.com>
2023-03-06 04:36:50 -0800

    rough draft of Product_Page Component

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import { Routes, Route} from 'react-router-dom';
 import Header from './Components/Header/Header';
-import Lifestyle from './pages/Accessories/Lifestyle';
+import Crewneck from './pages/Apparel/Tops/crewneck';
 
 function App() {
     return (
         <>
             <Header />
-            <Lifestyle />
+            <Crewneck />
         </>
     );
     }
---
+++ my-app/src/Components/Product_Page/product_page.jsx
@@ -0,0 +1,163 @@
+import React from "react";
+import "./product_page.css";
+import minus from "../../assets/minus.png";
+import plus from "../../assets/plus.png";
+
+const Product_Page = ({path, item, related_1, related_2, related_3, related_4}) => {
+    return (
+        <div className = "product-page-container">
+            <div className = "path-container">
+                <div className = "path-home-container">
+                    <h6 /*change to a link*/ className = "path-home">Home</h6>
+                </div>
+    
+                <div className = "path-category-container">
+                    <h6 /*change to a link*/ className = "path-category">{path.category}</h6>
+                </div>
+
+                <div className = "path-type-container">
+                    <h6 /*change to a link*/ className = "path-type">{path.type}</h6>
+                </div>
+
+                <div className = "path-item-container">
+                    <h6 /*change to a link*/ className = "path-item">{path.item}</h6>
+                </div>
+            </div>
+
+            <div className = "item-container">
+                <img className = "item-image" src = {item.img} alt = {item.name} />
+
+                <div className = "item-details">
+                    <div className = "item-name-container">
+                        <h1 className = "item-name">{item.name}</h1>
+                    </div>
+
+                    <div className = "item-price-container">
+                        <h2 className = "item-price">{item.price}</h2>
+                    </div>
+
+                    <div className = "line-container">
+                        <div className = "line"></div>
+                    </div>
+
+                    /* Needs an if-else for one size items */
+                    <div className = "size-container">
+                        <div className = "size-text-container">
+                            <h2 className = "size-text">"Size"</h2>
+                        </div>
+
+                        <div className = "size-xs-container">
+                            <h3 className = "size-xs">"XS"</h3>
+                        </div>
+
+                        <div className = "size-s-container">
+                            <h3 className = "size-s">"S"</h3>
+                        </div>
+
+                        <div className = "size-m-container">
+                            <h3 className = "size-m">"M"</h3>
+                        </div>
+    
+                        <div className = "size-l-container">
+                            <h3 className = "size-l">"L"</h3>
+                        </div>
+
+                        <div className = "size-xl-container">
+                            <h3 className = "size-xl">"XL"</h3>
+                        </div>
+
+                        <div className = "size-2xl-container">
+                            <h3 className = "size-2xl">"2XL"</h3>
+                        </div>
+                    </div>
+
+                    <div className = "quantity-container">
+                        <div className = "quantity-minus-container">
+                            <img className = "quantity-minus" src = {minus} alt = "minus" />
+                            <div className = "quantity-minus-container-border"></div>
+                        </div>
+
+                        /*Needs to be respond based on whether the minus or plus button is pressed*/
+                        <div className = "quantity-number-container-container">
+                            <div className = "quantity-number-container">
+                                <h3 className = "quantity-number">"1"</h3>
+                            </div>
+                            <div className = "quantity-number-container-container-border"></div>
+                        </div>
+
+                        <div className = "quantity-plus-container">
+                            <img className = "quantity-plus" src = {plus} alt = "plus" />
+                            <div className = "quantity-plus-container-border"></div>
+                        </div>
+                    </div>
+
+                    <div className = "add-to-cart-container">
+                        <h2 className = "add-to-cart">"Add to Cart"</h2>
+                    </div>
+
+                    <div className = "item-info-container">
+                        <p className = "item-info">{item.info}</p>
+                    </div>
+                </div>
+            </div>
+
+            <div className = "related-items-container">
+                <div className = "related-items-title-container">
+                    <h2 className = "related-items-title">"Related Items"</h2>
+                </div>
+
+                <div className = "related-items-row">
+                    <div className = "related-items-1-container">
+                        <img className = "related-items-1-image" src = {related_1.image} alt = {related_1.name} />
+                        
+                        <div className = "related-items-1-name-container">
+                            <h3 className = "related-items-1-name">{related_1.name}</h3>
+                        </div>
+
+                        <div className = "related-items-1-price-container">
+                            <h4 className = "related-items-1-price">{related_1.price}</h4>
+                        </div>
+                    </div>
+
+                    <div className = "related-items-2-container">
+                        <img className = "related-items-2-image" src = {related_2.image} alt = {related_2.name} />
+                        
+                        <div className = "related-items-2-name-container">
+                            <h3 className = "related-items-2-name">{related_2.name}</h3>
+                        </div>
+
+                        <div className = "related-items-2-price-container">
+                            <h4 className = "related-items-2-price">{related_2.price}</h4>
+                        </div>
+                    </div>
+
+                    <div className = "related-items-3-container">
+                        <img className = "related-items-3-image" src = {related_3.image} alt = {related_3.name} />
+                        
+                        <div className = "related-items-3-name-container">
+                            <h3 className = "related-items-3-name">{related_3.name}</h3>
+                        </div>
+
+                        <div className = "related-items-3-price-container">
+                            <h4 className = "related-items-3-price">{related_3.price}</h4>
+                        </div>
+                    </div>
+
+                    <div className = "related-items-4-container">
+                        <img className = "related-items-4-image" src = {related_4.image} alt = {related_4.name} />
+                        
+                        <div className = "related-items-4-name-container">
+                            <h3 className = "related-items-4-name">{related_4.name}</h3>
+                        </div>
+
+                        <div className = "related-items-4-price-container">
+                            <h4 className = "related-items-4-price">{related_4.price}</h4>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Product_Page;
