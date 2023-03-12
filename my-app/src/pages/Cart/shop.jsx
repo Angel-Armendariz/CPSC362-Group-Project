92f25f7ba6b62f2e4d09c759b6aef097d3ee6a81
Angel Armendariz <angela126509@gmail.com>
2023-03-12 15:38:48 -0700

    removed unneeded code

--- my-app/src/pages/Cart/shop.jsx
+++ my-app/src/pages/Cart/shop.jsx
@@ -4,16 +4,12 @@ import { Product } from "./product";
 
 export const Shop = () => {
   return (
-    <div className="shop">
-      <div className="shopTitle">
-        <h1>PedroTech Shop</h1>
-      </div>
-
+    <>
       <div className="products">
         {PRODUCTS.map((product) => (
           <Product data={product} />
         ))}
       </div>
-    </div>
+    </>
   );
 };
